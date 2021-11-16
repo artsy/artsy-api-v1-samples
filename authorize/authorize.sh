e00b65de787d49f9e97787b2d94bb20817a66c33
Anson Wang <ansor4@gmail.com>
2021-11-16 16:57:21 -0500

    Make recommendation clear

--- authorize/authorize.sh
+++ authorize/authorize.sh
@@ -2,7 +2,7 @@
 
 # For testing/staging, change urls to stagingapi.artsy.net instead of api.artsy.net
 
-# Option 1: Generate access token with redirect url
+# Option 1 (Recommended): Generate access token with redirect url
 
 # 1. First direct your users to https://api.artsy.net/oauth2/authorize?client_id=${client_id}&redirect_uri=${your_website}&response_type=code
 # 2. Extract the code from the query string after the redirect
@@ -11,7 +11,11 @@ curl -X POST https://api.artsy.net/oauth2/access_token \
      -H 'Content-Type: application/json' \
      -d "{ \"code\": \"${code}\", \"client_id\": \"${client_id}\", \"client_secret\": \"${client_secret}\", \"grant_type\": \"authorization_code\" }"
 
-# Option 2: Generate access token with user
+# Option 2: Generate access token for offline application
+
+# Note: This is meant for applications that are not hosted online, and therefore do not have a redirect url.
+#       For example, if your application runs locally on your client's desktop, this may be the only way for you to generate an access token if your desktop application does not support a web flow.
+#       For all other applications, we do not recommend using this to generate a token, the first option is both more secure and seamless to the partner. 
 
 curl -X POST https://api.artsy.net/oauth2/access_token \
      -H 'Content-Type: application/json' \
